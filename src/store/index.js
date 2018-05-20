bcbfb385e0f9fcf5ac184c1839cfc0ae2435a6f2
Matt Pedersen <mpedersen@mindrocketdesign.com>
2018-05-20 10:46:22 -0600

    added redux devtools extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
 import { stationApp } from '../reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(stationApp);
+export const store = createStore(stationApp, composeWithDevTools());
